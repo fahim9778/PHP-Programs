da68df38639df91605f57a91184eb1e128a0eb0f
MD. Gazzali Fahim <fahim9778@gmail.com>
2018-11-16 00:30:22 +0600

    First Commit :)

---
+++ views/projects.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MD. Fakhruddin Gazzali
+ * Date: 15-Nov-18
+ * Time: 9:08 PM
+ */
+return "<h1>Projects I've worked on</h1>
+<ul>
+<li>Soon It'll be updated</li>
+</ul>";
