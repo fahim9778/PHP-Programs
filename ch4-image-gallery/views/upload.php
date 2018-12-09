55abc1916d2d0fe66295d1bc88cf6cde141a50b9
MD. Gazzali Fahim <fahim9778@gmail.com>
2018-12-09 11:40:50 +0600

    Testing Upload

--- ch4-image-gallery/views/upload.php
+++ ch4-image-gallery/views/upload.php
@@ -6,5 +6,17 @@
  * Time: 7:07 PM
  */
 // code for uploading images
-$output = include_once "views/upload-form.php";
+$fileIsUploaded = isset($_GET['new-image']);    // check if file is uploaded via upload-form
+if($fileIsUploaded)
+    $output = upload();    // calling function 'upload'
+else
+    $output = include_once "views/upload-form.php";     // if not, then run to default
 return $output;
+
+//new function to control upload mechanism
+function upload()
+{
+    $out = "<pre>print_r($_FILES, true)</pre>";
+
+    return $out;
+}
